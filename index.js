22bcc687b1dfdee3b1026c054bbb1e510319a2f2
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-15 14:17:40 -0400

    javaCsript added to our index file

---
+++ index.js
@@ -0,0 +1,10 @@
+const sideMedu = document.querySelector("aside"); 
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = documen.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'block';
+})
+closeBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'none';
+})
